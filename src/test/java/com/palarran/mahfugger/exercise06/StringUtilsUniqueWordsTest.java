071c537a350c7c1083665ab5a62aa54785ef29ba
Jason Andersen <andersen.jason@gmail.com>
2015-04-02 20:00:57 -0500

    starting to write tests to read a file from a string

---
+++ src/test/java/com/palarran/mahfugger/exercise06/StringUtilsUniqueWordsTest.java
@@ -0,0 +1,41 @@
+package com.palarran.mahfugger.exercise06;
+
+/**
+ * Test the ability to read a text file and return a list of unique words from
+ * that file.
+ */
+public class StringUtilsUniqueWordsTest {
+
+    /*
+     * FIXME - make these tests pass.
+     */
+
+    /*
+     * ****** SPOILER ALERT - ONLY READ BELOW IF YOU'RE STUCK *******
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+}
