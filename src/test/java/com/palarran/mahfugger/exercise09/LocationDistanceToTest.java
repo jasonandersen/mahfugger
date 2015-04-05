748736dcfba969b843711a7c5ac1937ebb25521c
Jason Andersen <andersen.jason@gmail.com>
2015-04-05 09:31:49 -0700

    some last minute cleanup

--- src/test/java/com/palarran/mahfugger/exercise09/LocationDistanceToTest.java
+++ src/test/java/com/palarran/mahfugger/exercise09/LocationDistanceToTest.java
@@ -48,44 +48,4 @@ public class LocationDistanceToTest {
         testLocation = new Location(50f, 126f);
         assertEquals(7280106.2, seattle.distanceTo(testLocation), 0.1);
     }
-
-    /*
-     * EVERYTHING FROM HERE DOWN IS THROW AWAY CODE
-     */
-
-    private HaversineDistance dist = new HaversineDistance();
-
-    @Test
-    public void test() {
-        dist.distance(48.0, -123.0, 50.0, 126.0);
-    }
-
-    private class HaversineDistance {
-
-        /**
-         * @param args
-         * arg 1- latitude 1
-         * arg 2 - latitude 2
-         * arg 3 - longitude 1
-         * arg 4 - longitude 2
-         */
-        public void distance(Double lat1, Double lon1, Double lat2, Double lon2) {
-            final int R = 6371; // Radious of the earth
-            Double latDistance = toRad(lat2 - lat1);
-            Double lonDistance = toRad(lon2 - lon1);
-            Double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) +
-                    Math.cos(toRad(lat1)) * Math.cos(toRad(lat2)) *
-                    Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
-            Double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            Double distance = R * c * 1000;
-
-            System.out.println("The distance between two lat and long is::" + distance);
-
-        }
-
-        private Double toRad(Double value) {
-            return value * Math.PI / 180;
-        }
-
-    }
 }
