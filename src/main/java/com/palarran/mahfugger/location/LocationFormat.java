1620ea2ab37b7f5ca79b579942db09ed1febf576
Jason Andersen <andersen.jason@gmail.com>
2015-04-13 13:48:29 -0500

    added location formatting tests

---
+++ src/main/java/com/palarran/mahfugger/location/LocationFormat.java
@@ -0,0 +1,43 @@
+package com.palarran.mahfugger.location;
+
+/**
+ * Different formatting options for {@link Location} instances.
+ */
+public enum LocationFormat {
+    /**
+     * Example: 47° 36' 23.2"N 127° 12' 24.6"W
+     */
+    DEGREES_MINS_SECS {
+        @Override
+        public String format(Location location) {
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+    },
+    /**
+     * Example: 47° 36.425'N 127° 12.475'W
+     */
+    DEGREES_MINS {
+        @Override
+        public String format(Location location) {
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+    },
+    /**
+     * Example: 47.582°N 127.2391°W
+     */
+    DEGREES {
+        @Override
+        public String format(Location location) {
+            throw new UnsupportedOperationException("not implemented yet");
+        }
+    };
+
+    /**
+     * Formats the location based on the specified format.
+     * @param location
+     * @return a formatted string for the location
+     * @throws IllegalArgumentException when location is null
+     */
+    public abstract String format(Location location);
+
+}
---
+++ src/test/java/com/palarran/mahfugger/exercise16/LocationFormatTest.java
@@ -0,0 +1,85 @@
+package com.palarran.mahfugger.exercise16;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.palarran.mahfugger.location.Location;
+import com.palarran.mahfugger.location.LocationFormat;
+
+/**
+ * Test to ensure Locations are properly formatted.
+ */
+public class LocationFormatTest {
+
+    /*
+     * LocationFormat is an enum that represents the only possible location formatting options
+     * for a Location class. Enum are objects however and they can contain both data and behavior.
+     * Each enumeration in an enum class is it's own instance of that class. So for LocationFormat,
+     * we're using the different instances to contain the algorithm for the formatting. This is a
+     * clean way to house Location formatting code in one spot without kludgy if/else blocks or 
+     * select blocks.
+     * 
+     * FIXME - make these tests pass!
+     */
+
+    private LocationFormat format;
+
+    private Location location = new Location(47.89234, -123.456);
+
+    @Test
+    public void testDegMinSec() {
+        format = LocationFormat.DEGREES_MINS_SECS;
+        assertEquals("47° 53' 32.424\"N 123° 27' 21.6\"W", format.format(location));
+    }
+
+    @Test
+    public void testDegMin() {
+        format = LocationFormat.DEGREES_MINS;
+        assertEquals("47° 53.5404'N 123° 27.36'W", format.format(location));
+    }
+
+    @Test
+    public void testDeg() {
+        format = LocationFormat.DEGREES;
+        assertEquals("47.89234°N 123.456°W", format.format(location));
+    }
+
+    /*
+     * ************************** SPOILER ALERT **************************
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * Refactor Location.toString() method to use LocationFormat.DEG_MIN_SEC.format(this) 
+     * That way this logic only ever exists in a single place. Any logic that exists in
+     * more than one place is a way for defects to propogate through your code in a way
+     * that is unbelievably frustrating to fix.
+     * 
+     * 
+     */
+}
