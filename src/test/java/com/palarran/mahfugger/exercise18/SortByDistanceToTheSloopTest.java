e9fed1c37209a3672908680409b70c16b94d8cec
Jason Andersen <andersen.jason@gmail.com>
2015-08-13 11:43:11 -0700

    getting some project file changes in place

---
+++ src/test/java/com/palarran/mahfugger/exercise18/SortByDistanceToTheSloopTest.java
@@ -0,0 +1,17 @@
+package com.palarran.mahfugger.exercise18;
+
+/**
+ * Test the ability to sort a collection of Locations by their distance from 
+ * the Sloop Tavern.
+ */
+public class SortByDistanceToTheSloopTest {
+
+    /*
+     * FIXME - make these tests pass.
+     * 
+     * Take a look at 
+     *     Comparator<TrackPoint>
+     *     Collections.sort()
+     */
+
+}
