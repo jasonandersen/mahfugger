844beaa3f69e747b72ffe4df7edd057e75f7042d
Jason Andersen <andersen.jason@gmail.com>
2015-09-09 08:50:09 -0700

    Updated the formatting to make the code a little more readable.

--- src/main/java/com/palarran/mahfugger/util/StringUtils.java
+++ src/main/java/com/palarran/mahfugger/util/StringUtils.java
@@ -3,6 +3,7 @@ package com.palarran.mahfugger.util;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.log4j.Logger;
 
 /**
@@ -10,47 +11,50 @@ import org.apache.log4j.Logger;
  */
 
 public class StringUtils {
-	 private static final Logger log = Logger.getLogger(StringUtils.class);
-	 /**
-     * Private constructor.
-     */
-	
+    private static final Logger log = Logger.getLogger(StringUtils.class);
+
+    /**
+    * Private constructor.
+    */
+
     private StringUtils() {
         /*
          * This is a private constructor. By making the constructor private, that prevents anyone
          * from instantiating this class. This class will be comprised of static methods only and
          * therefore would not make sense to instantiate.
          */
-    	
-    }	           
+
+    }
+
     /**
      * @param original the string to reverse
      * @return a new string that contains all of the characters from the original string in
      *      reverse order.
      * @throws IllegalArgumentException if original is null     
      */
-   
+
     public static String reverse(String original) {
-    	if (original == null) {
+        if (original == null) {
             throw new IllegalArgumentException("Oopsie");
         }
-    	    	
-       	 String flipIt = new String(original);
-     	 String expectedOutput = "";
-     	 String actualOutput = "";
-     
-     	 int input = flipIt.length();
-     	
-     	 for (int i = input - 1 ; i >= 0 ; i-- )
-             expectedOutput = expectedOutput + flipIt.charAt(i);
-     	 	 log.info("Output of flipIt after loop = " + flipIt);
-     	 	 log.info("Length of input is: " + input);
-        	 actualOutput = expectedOutput;
-        	 log.info("Expected Output = " + expectedOutput);
-        	 log.info("Actual Output = " + actualOutput);
-        	 return actualOutput;
+
+        String flipIt = new String(original);
+        String expectedOutput = "";
+        String actualOutput = "";
+
+        int input = flipIt.length();
+
+        for (int i = input - 1; i >= 0; i--) {
+            expectedOutput = expectedOutput + flipIt.charAt(i);
+        }
+        log.info("Output of flipIt after loop = " + flipIt);
+        log.info("Length of input is: " + input);
+        actualOutput = expectedOutput;
+        log.info("Expected Output = " + expectedOutput);
+        log.info("Actual Output = " + actualOutput);
+        return actualOutput;
     }
-    
+
     /**
      * @param paragraph the string to break down into words
      * @return a list containing the words in the paragraph in the order in which they appear
